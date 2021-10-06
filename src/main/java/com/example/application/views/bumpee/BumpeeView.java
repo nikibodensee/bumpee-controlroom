3c08a0078c2642fa633aebe69a7d7bc390322f57
start.vaadin.com <no-reply@vaadin.com>
2021-10-06 15:29:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/bumpee/BumpeeView.java
@@ -0,0 +1,32 @@
+package com.example.application.views.bumpee;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Bumpee")
+@Route(value = "hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class BumpeeView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public BumpeeView() {
+        addClassName("bumpee-view");
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        add(name, sayHello);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+    }
+
+}
